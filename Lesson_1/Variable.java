771d3cece7d8bca8ffa5f7db7963e5a987febe04
Alexander Slepchenko <asander@list.ru>
2020-09-17 14:43:03 +0300

    implement HW_1

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte core = 8;
+		short hdd = 1000;
+		int win = 10;
+		long ram = 65536;
+		float e = -1234;
+		double ghz = 3.6;
+		char g = 123;
+		boolean monitor = true;
+
+		System.out.println("Кол-во ядер - " + core + " " + ghz + " GHz. ОЗУ " + ram + " Операционная система Win " + win + 
+		" Жесткий диск " + hdd + " GB" + ". " + monitor + " Монитор." + " В кармане " + e + " денег.");
+	}
+}
