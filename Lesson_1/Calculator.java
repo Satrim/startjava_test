17a3460ec7518fa57f4d97dc5e842cbec623b408
Alexander Slepchenko <asander@list.ru>
2020-09-22 11:11:06 +0300

    implement HR_1.4

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    public static void main (String[] args) {
+        double number1 = 10;
+        double number2 = 3;
+        char mathOperation = '+';
+        double result = 0;
+        if (mathOperation == '+') {
+            result = number1 + number2;
+        } else if (mathOperation == '-') {
+            result = number1 - number2;
+        } else if (mathOperation == '*') {
+            result = number1 * number2;
+        } else if (mathOperation == '/') {
+            result = number1 / number2;
+        } else if (mathOperation == '%') {
+            result = number1 % number2;
+        } else if (mathOperation == '^') {
+            result = 1;
+            for (int i = 0; i < number2; i ++) {
+                result = result * number1;
+            }
+        }
+        System.out.println(result);
+    }
+}
