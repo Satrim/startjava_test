6d8fca42653d0808378c04f51a80c636f72aa3ce
Alexander Slepchenko <asander@list.ru>
2020-09-23 10:51:58 +0300

    Correcterd

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,13 +1,19 @@
 public class MyFirstGame {
-    public static void main (String[] args) {
-        int trueNumber = 5;
-        int attempt = 8;
-        if (attempt < trueNumber) {
-            System.out.println("Введенное вами число меньше того, что загадал компьютер");
-        } else if (attempt > trueNumber) {
-            System.out.println("Введенное вами число больше того, что загадал компьютер");
-        } else if (attempt == trueNumber) {
-            System.out.println("Вы угадали!");
+    public static void main(String[] args) {
+        int trueNumber = 25;
+        int attempt = 5;
+
+        System.out.println(attempt);
+        while(attempt != trueNumber) {
+            if (attempt < trueNumber) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                attempt += 5;
+            } else if (attempt > trueNumber) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер");
+                attempt -= 5;
+            }
+            System.out.println(attempt);
         }
+        System.out.println("Вы угадали!");
     }
 }
