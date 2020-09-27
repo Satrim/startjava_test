1d2273a3e002c82a861e7212368178de316e6eea
Satrim <59724286+Satrim@users.noreply.github.com>
2020-09-27 22:24:56 +0300

    Implement HW_2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+	String gender = "mail";
+    String name = "John";
+    int height = 182;
+    int weight = 85;
+    int age = 27;
+
+    void move() {
+        System.out.println("Я иду");
+    }
+
+    void sit() {
+        System.out.println("Я сижу");   
+    }
+
+    void run() {
+        System.out.println("Я бегу");
+    }
+
+    void speak() {
+        System.out.println("Я говорю");
+    }
+
+    void learn() {
+    	System.out.println("Я учусь");
+    }
+}
