f941ae311f4335bbdbbde2ef9be514493bbf1f35
Satrim <59724286+Satrim@users.noreply.github.com>
2020-10-12 14:05:43 +0300

    Correcterd

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -8,25 +8,28 @@ public class GuessNumber {
     private Random random = new Random();
     private int hiddenNumber = random.nextInt(101);
     Scanner scanner = new Scanner(System.in);
-    private int amountAttemptPlayer;
-    private int amountAttemptGeneral = 10;
+    private static final int COUNT_ATTEMPT = 10;
 
     public GuessNumber(Player[] players) {
         this.players = players;
     }
 
     public void run() {
+        System.out.println(hiddenNumber);
         for(int i = 0; i < 10; i++) {
             int number;
 
             for(int j = 0; j < players.length; j++) {
-                System.out.println("Игрок " + players[j].getName() + " введите число. У вас осталось попыток: " + (amountAttemptGeneral - i));
+                System.out.println("Игрок " + players[j].getName() + " введите число. У вас осталось попыток: " + (COUNT_ATTEMPT - i));
                 number = scanner.nextInt();
-                amountAttemptPlayer = i;
+                players[j].getNumberAttempt(i + 1);
                 players[j].setNumber(number, i);
                 if (checkNumber(number)) {
                     System.out.println("Игрок " + players[j].getName() + " угадал число " + number + " с " + (i + 1) + " попытки");
-                    showEnteredNums();
+                    players[j].getNumberAttempt(i + 1);
+                    players[j].setNumber(number, i);
+                    showEnteredNumber();
+                    clearEnteredNumber(i + 1);
                     return;
                 }
                 if (i == 9) {
@@ -34,30 +37,29 @@ public class GuessNumber {
                 }
             }
         }
-        showEnteredNums();
-        clearEnteredNums(amountAttemptPlayer);
+        showEnteredNumber();
+        clearEnteredNumber(COUNT_ATTEMPT); // Поскольку никто не угадал, массив полностью заполнен десятью цифрами
     }
 
     private boolean checkNumber(int number) {
-        boolean isGuess = false;
-        if (hiddenNumber == number) {
-            isGuess = true;
+        if (hiddenNumber == number) { //Убрал переменную boolean
+            return true;
         }
         System.out.println("Введенное вами число " + number + (hiddenNumber > number ? " меньше" : " больше") + " того, что загадал компьютер");
-        return isGuess;
+        return false;
     }
 
-    private void showEnteredNums() {
+    private void showEnteredNumber() {
         for (Player player : players) {
             System.out.print("Числa, введенные игроком " + player.getName() + ": ");
-            for (int number : player.getAttempts(amountAttemptPlayer + 1)) {
+            for (int number : player.getAttempts()) {
                 System.out.print(number + " ");
             }
             System.out.println();
         }
     }
 
-    public void clearEnteredNums(int index) {
+    public void clearEnteredNumber(int index) {
         for (Player player : players) {
             player.clear(index);
         }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Player {
     private String name;
     private int[] attempts = new int[10];
+    private int numberAttempt;
 
     public Player(String name) {
         this.name = name;
@@ -14,15 +15,20 @@ public class Player {
         return name;
     }
 
-    public int[] getAttempts(int index) {
-        return Arrays.copyOf(attempts, index);
+    public int[] getAttempts() {
+        return Arrays.copyOf(attempts, numberAttempt);
+    }
+
+    public void setNumber(int number, int index) {
+        attempts[index] = number;
+    }
+
+    public void getNumberAttempt(int numberAttempt) {
+        this.numberAttempt = numberAttempt;
     }
 
     public void clear(int index) {
         Arrays.fill(attempts, 0, index, 0);
     }
 
-    public void setNumber(int number, int index) {
-        attempts[index] = number;
-    }
 }
